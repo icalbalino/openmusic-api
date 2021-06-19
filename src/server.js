d9a6ac8ed1bd65910da9accba1552cad33dc9d03
if0balino <balino.ical@gmail.com>
2021-06-19 19:20:01 +0700

    config .env

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable spaced-comment */
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 //const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
