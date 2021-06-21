93cea3fbfcc783fbedb80425fd155b3842929e9c
if0balino <balino.ical@gmail.com>
2021-06-21 09:46:19 +0700

    add plugin in server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable padded-blocks */
+/* eslint-disable no-console */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
 /* eslint-disable spaced-comment */
+
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-//const routes = require('./routes');
+const musics = require('./api/musics');
+const MusicsService = require('./services/inMemory/MusicsService');
+
 
 const init = async () => {
+  const musicsService = new MusicsService();
+  
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -16,9 +27,17 @@ const init = async () => {
     },
   });
 
-  //server.route(routes);
+  await server.register({
+    plugin: musics,
+    options: {
+      service: musicsService,
+    },
+  });
+ 
   await server.start();
-  //console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
+
 };
+
 
 init();
