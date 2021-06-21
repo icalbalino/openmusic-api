3b4fe689da6e38d62964f8f594aece965b060bab
if0balino <balino.ical@gmail.com>
2021-06-21 12:15:02 +0700

    add data validation with joi

---
+++ src/validator/musics/schema.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-multiple-empty-lines */
+
+const Joi = require('joi');
+
+const MusicPayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  year: Joi.number().required(),
+  performer: Joi.string().required(),
+  genre: Joi.string(),
+  duration: Joi.number(),
+});
+
+
+module.exports = { MusicPayloadSchema };
