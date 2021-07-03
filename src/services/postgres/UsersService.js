9afb53c78fc544a9dfbab512afb23dc913d7c934
if0balino <balino.ical@gmail.com>
2021-07-04 00:14:32 +0700

    add (f) async addUser

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,10 +1,14 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable padded-blocks */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable no-multiple-empty-lines */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
 
 
+const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 
 
@@ -13,7 +17,23 @@ class UsersService {
     this._pool = new Pool();
   }
 
-
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+ 
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
+      values: [id, username, hashedPassword, fullname],
+    };
+ 
+    const result = await this._pool.query(query);
+ 
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
+    }
+    return result.rows[0].id;
+  }
 
   async verifyNewUsername(username) {
     const query = {
@@ -27,4 +47,8 @@ class UsersService {
       throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
     }
   }
+
 }
+
+
+module.exports = UsersService;
